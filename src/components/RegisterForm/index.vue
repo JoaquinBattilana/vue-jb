3fd45af14b80a90fbc9358bb3e30d261318e2770
JoaquinBattilana <battilanajn@gmail.com>
2020-03-08 20:53:22 -0300

    created basic form

--- src/components/RegisterForm/index.vue
+++ src/components/RegisterForm/index.vue
@@ -1,9 +1,36 @@
 <template lang="pug">
-    h1 Hola
+  form(class='asd')
+    img(src='@/assets/image.png')
+    label First name
+    input(name='firstName' v-model='searchForm.firstName')
+    label Last name
+    input(name='lastName' v-model='searchForm.lastName')
+    label Email
+    input(name='email' v-model='searchForm.email' type='email')
+    label Password
+    input(name='password' v-model='searchForm.password' type='password')
 </template>
 
 <script>
 
-export default {}
+export default {
+  data () {
+    return {
+      searchForm: {
+        firstName: '',
+        lastName: '',
+        email: '',
+        password: ''
+      }
+    }
+  }
+}
 
 </script>
+
+<style lang='scss'>
+  .asd {
+    display: flex;
+    flex-direction: column;
+  }
+</style>
