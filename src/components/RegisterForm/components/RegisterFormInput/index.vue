a38ba92f59158e90bfaaf53f925043a4a3e0aa6d
JoaquinBattilana <battilanajn@gmail.com>
2020-03-22 23:23:15 -0300

    styled register form input

--- src/components/RegisterForm/components/RegisterFormInput/index.vue
+++ src/components/RegisterForm/components/RegisterFormInput/index.vue
@@ -1,6 +1,6 @@
 <template lang="pug">
-  label(:for='name').search-form-label {{label}}
-    input.search-form-input(:name='name' :id='name' :value='value' @input='updateValue' :type='type')
+  label.search-form-label {{label}}
+    input.search-form-input(:name='name' :value='value' @input='updateValue' :type='type')
 </template>
 
 <script>
@@ -24,11 +24,11 @@ export default {
     align-self: flex-start;
     margin-bottom: 10px;
     width: 100%;
+    position: relative;
 
-  .search-form-input {
-    height: 20px;
-    width: 100%;
+    .search-form-input {
+      height: 20px;
+      width: 100%;
+    }
   }
-  }
-
 </style>
