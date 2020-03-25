20f2bcbd746b36417e6577fa73af7e78212ff732
JoaquinBattilana <battilanajn@gmail.com>
2020-03-25 11:51:45 -0300

    added i18n support

--- src/components/RegisterForm/index.vue
+++ src/components/RegisterForm/index.vue
@@ -1,11 +1,11 @@
 <template lang="pug">
   form.flex-column.search-form.p-3(@submit.prevent='handleSubmit')
     img.max-full-width.m-bottom-5(src='@/assets/image.png')
-    register-form-input(v-show='register' name='registerFormFirstName' label='First name' v-model='searchForm.firstName' )
-    register-form-input(v-show='register' name='registerFormLastName' label='Last name' v-model='searchForm.lastName')
-    register-form-input(name='registerFormEmail' label='Email' v-model='searchForm.email' :errors='searchFormErrors.email')
-    register-form-input(name='registerFormPassword' label='Password' v-model='searchForm.password' type='password' :errors='searchFormErrors.password')
-    register-form-input(v-show='register' name='registerFormPasswordConfirmation' label='Password Confirmation' v-model='searchForm.passwordConfirmation' type='password')
+    register-form-input(v-show='register' name='registerFormFirstName' :label="$t('registerForm.firstNameLabel')" v-model='searchForm.firstName' )
+    register-form-input(v-show='register' name='registerFormLastName' :label="$t('registerForm.lastNameLabel')" v-model='searchForm.lastName')
+    register-form-input(name='registerFormEmail' :label="$t('registerForm.emailLabel')" v-model='searchForm.email' :errors='searchFormErrors.email')
+    register-form-input(name='registerFormPassword' :label="$t('registerForm.passwordLabel')" v-model='searchForm.password' type='password' :errors='searchFormErrors.password')
+    register-form-input(v-show='register' name='registerFormPasswordConfirmation' :label="$t('registerForm.passwordConfirmationLabel')" v-model='searchForm.passwordConfirmation' type='password')
     button.smooth-edges.form-button.primary-color(type=submit) {{register ? 'Sign Up' : 'Login'}}
     .grey-separator
     router-link.smooth-edges.form-button.secondary-color(tag='button' :to="register ? '/login' : '/sign-up' ") {{register ? 'Login' : 'Sign up'}}
---
+++ src/config/i18n/registerForm.js
@@ -0,0 +1,7 @@
+export default {
+  firstNameLabel: 'Nombre',
+  lastNameLabel: 'Apellido',
+  emailLabel: 'Email',
+  passwordLabel: 'Contraseña',
+  passwordConfirmationLabel: 'Confirmar contraseña'
+}
