66f653c863940ca6275b449de915ba06b2a34755
JoaquinBattilana <battilanajn@gmail.com>
2020-03-25 08:50:50 -0300

    styled navbar

---
+++ src/components/Navbar/index.vue
@@ -0,0 +1,26 @@
+<template lang="pug">
+  nav.flex-row.full-width.navbar-container
+    img.navbar-image(src='@/assets/image.png')
+    button.logout-button logout
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang='scss'>
+.navbar-container {
+  align-items: center;
+  padding: 0 20%;
+  border-bottom: 1px solid grey;
+
+  .navbar-image {
+    max-height: 30px;
+  }
+
+  .logout-button {
+    margin-left: auto;
+  }
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -2,13 +2,14 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Register from '@/views/Register'
 import Login from '@/views/Login'
+import Home from '@/views/Home'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    redirect: '/login'
+    component: Home
   },
   {
     path: '/sign-up',
