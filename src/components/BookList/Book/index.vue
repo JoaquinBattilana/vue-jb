a36632aeb7267ca32e57a589d5a394bd6625f9a7
JoaquinBattilana <battilanajn@gmail.com>
2020-03-25 09:34:20 -0300

    created book list and service call

---
+++ src/components/BookList/Book/index.vue
@@ -0,0 +1,25 @@
+<template lang="pug">
+  li
+    img(src='img')
+    h2 {{title}}
+    span {{author}}
+</template>
+
+<script>
+export default {
+  props: {
+    img: {
+      type: String,
+      required: true
+    },
+    title: {
+      type: String,
+      required: true
+    },
+    author: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
